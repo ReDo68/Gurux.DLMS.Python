2fc8d3555b44bb1004e1271b216906d08f06a0fa
ReDo68 <re.dolatabadi@gmail.com>
2021-09-01 08:45:55 +0430

    Back to serial version

--- Gurux.DLMS.Client.Example.python/main.py
+++ Gurux.DLMS.Client.Example.python/main.py
@@ -125,11 +125,11 @@ class sampleclient():
             print("Ended. Press any key to continue.")
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
     # arg_reza = ['Gurux.DLMS.Client.Example.python/main.py', '-S', '/dev/ttyUSB0:19200:8Even1', '-g', '1.0.1.8.0.255:2',
     #             '-c', '1', '-s', '17493', '-a', 'HighGMac', '-t', 'Verbose', '-T', '4D4D4D0000000001', '-v',
     #             '0.0.43.1.0.255', '-C', 'AuthenticationEncryption', '-o',
     #             '/home/reza/Documents/Project/dlms/device.xml', '-G', 'sepanta']
-    #
+
     # sampleclient.main(arg_reza)
-    # # sampleclient.main(sys.argv)
+    sampleclient.main(sys.argv)
