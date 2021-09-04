1b908ebfb885bbd874b7c5c597acf37e77faa1d3
ReDo68 <re.dolatabadi@gmail.com>
2021-09-04 14:03:23 +0430

    Ready for server

--- Gurux.DLMS.Client.Example.python/test.py
+++ Gurux.DLMS.Client.Example.python/test.py
@@ -10,13 +10,18 @@ from main import sampleclient
 #             '0.0.43.1.0.255', '-C', 'AuthenticationEncryption', '-o',
 #             'device.xml', '-G', 'sepanta']
 
+arg_reza = ['Gurux.DLMS.Client.Example.python/main.py', '-h', 'localhost', '-p', '7370', '-g', '1.0.1.8.0.255:2',
+            '-c', '1', '-s', '17493', '-a', 'HighGMac', '-t', 'Verbose', '-T', '4D4D4D0000000001', '-v',
+            '0.0.43.1.0.255', '-C', 'AuthenticationEncryption', '-o',
+            'device.xml', '-G', 'sepanta']
+
 # arg_reza = ['Gurux.DLMS.Client.Example.python/main.py', '-S', '/dev/ttyUSB0:9600:8None1' , '-g', '1.0.1.8.0.255:2',
 #             '-c', '1', '-s', '17493', '-a', 'HighGMac', '-t', 'Verbose', '-T', '4D4D4D0000000001', '-v',
 #             '0.0.43.1.0.255', '-C', 'AuthenticationEncryption', '-o', 'tfc.xml']
 
-arg_reza = ['Gurux.DLMS.Client.Example.python/main.py', '-S', '/dev/ttyUSB0:9600:8None1' , '-g', '1.0.15.8.0.255:2;1.0.15.8.0.255:3',
-            '-c', '1', '-s', '19369', '-a', 'HighGMac', '-t', 'Verbose', '-T', '4D4D4D0000000001', '-v',
-            '0.0.43.1.0.255', '-C', 'AuthenticationEncryption', '-o', 'afzar.xml']
+# arg_reza = ['Gurux.DLMS.Client.Example.python/main.py', '-S', '/dev/ttyUSB0:9600:8None1' , '-g', '1.0.15.8.0.255:2;1.0.15.8.0.255:3',
+#             '-c', '1', '-s', '19369', '-a', 'HighGMac', '-t', 'Verbose', '-T', '4D4D4D0000000001', '-v',
+#             '0.0.43.1.0.255', '-C', 'AuthenticationEncryption', '-o', 'afzar.xml']
 
 
 sampleclient.main(arg_reza)
