b5a79b775ff6f51e944273f2d09428275d98b689
ReDo68 <re.dolatabadi@gmail.com>
2021-09-04 15:22:37 +0430

    Ready for server

--- Gurux.DLMS.Client.Example.python/test.py
+++ Gurux.DLMS.Client.Example.python/test.py
@@ -1,5 +1,5 @@
 from main import sampleclient
-#
+
 # arg_reza = ['Gurux.DLMS.Client.Example.python/main.py', '-S', '/dev/ttyUSB0:19200:8Even1', '-g', '1.0.1.8.0.255:2',
 #             '-c', '1', '-s', '17493', '-a', 'HighGMac', '-t', 'Verbose', '-T', '4D4D4D0000000001', '-v',
 #             '0.0.43.1.0.255', '-C', 'AuthenticationEncryption', '-o',
