fa7b70e8bc4f5b01890e786b76dc93724b1889d8
ReDo68 <re.dolatabadi@gmail.com>
2021-09-13 23:18:26 +0430

    read v4- tfc

--- Gurux.DLMS.Client.Example.python/GXDLMSReader.py
+++ Gurux.DLMS.Client.Example.python/GXDLMSReader.py
@@ -159,7 +159,7 @@ class GXDLMSReader:
                     self.media.send(data)
             pos = 0
             try:
-                while not self.client.getData(rd[3:], reply, notify):
+                while not self.client.getData(rd[2:], reply, notify):
                     if notify.data.size != 0:
                         if not notify.isMoreData():
                             t = GXDLMSTranslator()
@@ -178,6 +178,7 @@ class GXDLMSReader:
                         # self.media.send(data, None)
                     rd.set(p.reply)
                     print(str(rd))  # to print anything that gw send
+                    print(str(rd[2:]))  # to print anything that gw send
                     p.reply = None
             except Exception as e:
                 self.writeTrace("RXgw: " + self.now() + "\t" + str(rd), TraceLevel.ERROR)  # R374-change it to gw
