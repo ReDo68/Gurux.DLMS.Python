604a6408bc20ccd57be48dd53c0a825c0dafb959
ReDo68 <re.dolatabadi@gmail.com>
2021-09-15 11:08:32 +0430

    V4 - merging to the server

--- Gurux.DLMS.Client.Example.python/main.py
+++ Gurux.DLMS.Client.Example.python/main.py
@@ -128,12 +128,62 @@ class sampleclient():
                     traceback.print_exc()
             print("Ended. Press any key to continue.")
 
+class ReadV4:
+    def __init__(self, meter_type, physical):
+        self.meter_type = meter_type  # 'tfc' 'afzar'
+        self.OBIS = '1.0.1.8.0.255:1;1.0.1.8.0.255:2;1.0.1.8.0.255:3'
+        # self.OBIS = '0.0.20.0.0.255:2;0.0.20.0.0.255:3;0.0.20.0.0.255:4;0.0.20.0.0.255:5;' \
+        #             '0.2.22.0.0.255:7;0.2.22.0.0.255:8'   Timming
+        self.device = 'meter'  # 'gw' 'meter'
+        self.media = 'Serial'  # 'TCP' 'Serial'
+        self.server_addr = str(16384+physical)    #'19369'  # 0x4000+physical(1000+sn_last_4digits)
 
-if __name__ == '__main__':
-    # arg_reza = ['Gurux.DLMS.Client.Example.python/main.py', '-S', '/dev/ttyUSB0:19200:8Even1', '-g', '1.0.1.8.0.255:2',
-    #             '-c', '1', '-s', '17493', '-a', 'HighGMac', '-t', 'Verbose', '-T', '4D4D4D0000000001', '-v',
-    #             '0.0.43.1.0.255', '-C', 'AuthenticationEncryption', '-o',
-    #             '/home/reza/Documents/Project/dlms/device.xml', '-G', 'sepanta']
+        self.client_addr = '1'
+        self.ip = 'localhost'  #'193.105.234.168'  'localhost'
+        self.port = '7370'
+        self.usb = "/dev/ttyUSB0"
 
-    # sampleclient.main(arg_reza)
-    sampleclient.main(sys.argv)
+    def read(self):
+        arg = ['Gurux.DLMS.Client.Example.python/main.py', '-c', self.client_addr, '-s', self.server_addr, '-a', 'HighGMac', '-t', 'Verbose',
+               '-T', '4D4D4D0000000001', '-v', '0.0.43.1.0.255', '-C', 'AuthenticationEncryption']
+
+
+        if self.device == 'gw':
+            self.usb = self.usb+":19200:8Even1"
+            arg = arg + ['-G', 'sepanta']
+        elif self.device == 'meter':
+            self.usb = self.usb + ":9600:8None1"
+        else:
+            print("Please choose between 'gw' and 'meter' mode in device")
+
+        if self.media == 'TCP':
+            arg = arg + ['-h', self.ip, '-p', self.port]
+        elif self.media == 'Serial':
+            arg = arg + ['-S', self.usb]
+        else:
+            print("Please choose between 'TCP' and 'Serial' mode in media")
+
+        if self.OBIS:
+            arg = arg + ['-g', self.OBIS]
+
+        if self.meter_type == 'tfc':
+            arg = arg + ['-o', 'tfc.xml']
+        elif self.meter_type == 'afzar':
+            arg = arg + ['-o', 'afzar.xml']
+        else:
+            print("Please choose a correct meter_type EX: afzar")
+
+        return arg
+
+# print(read_v4('tfc').read())
+# sampleclient.main(ReadV4('afzar', 2985).read())  #1110
+
+
+# if __name__ == '__main__':
+#     # arg_reza = ['Gurux.DLMS.Client.Example.python/main.py', '-S', '/dev/ttyUSB0:19200:8Even1', '-g', '1.0.1.8.0.255:2',
+#     #             '-c', '1', '-s', '17493', '-a', 'HighGMac', '-t', 'Verbose', '-T', '4D4D4D0000000001', '-v',
+#     #             '0.0.43.1.0.255', '-C', 'AuthenticationEncryption', '-o',
+#     #             '/home/reza/Documents/Project/dlms/device.xml', '-G', 'sepanta']
+#
+#     # sampleclient.main(arg_reza)
+#     sampleclient.main(sys.argv)
